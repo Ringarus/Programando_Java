dd5df9711a58ebbff63909df9e474d0644ccad0c
Valério <valcorr29@gmail.com>
2023-12-27 22:50:58 -0300

    Segundo commit

--- PrimeiroProjetoEmJava/src/App.java
+++ PrimeiroProjetoEmJava/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Meu primeiro código Java!");
         System.out.println("Bem vindo ao mundo Java!");
+        System.out.println("Bem vindo ao mundo Dois!");
     }
 }
