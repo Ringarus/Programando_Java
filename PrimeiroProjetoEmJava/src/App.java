b89d7e015ad26a5d7bb215a200826be62f2d6a73
Valério <valcorr29@gmail.com>
2023-12-27 22:46:40 -0300

    Primeira mudança do codigo .java

--- PrimeiroProjetoEmJava/src/App.java
+++ PrimeiroProjetoEmJava/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Meu primeiro código Java!");
+        System.out.println("Bem vindo ao mundo Java!");
     }
 }
