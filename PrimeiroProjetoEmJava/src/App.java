3668dc1d37fa98282ac0e15720c1ead4622ffec4
Valério <valcorr29@gmail.com>
2024-01-15 02:13:36 -0300

    Realizando organização no diretório

--- PrimeiroProjetoEmJava/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! Meu primeiro código Java!");
-        System.out.println("Bem vindo ao mundo Java!");
-        System.out.println("Bem vindo ao mundo Dois!");
-    }
-}
---
+++ PrimeiroProjetoEmJava/src/Aulas iniciais/Armazena.java
@@ -0,0 +1,15 @@
+public class Armazena {
+    /*
+    * Crie um programa que declare variáveis para armazenar seu 
+    * nome, idade e altura. Em seguida, imprima essas informações 
+    * formatadas no console. 
+    */
+
+    public static void main(String[] args) {
+        String nome = "Jhon";
+        int idade = 22;
+        double altura = 1.72;
+        System.out.printf("Oi, meu nome é %s, tenho %d anos e %fm de altura\n", nome, idade, altura);
+
+    }
+}
