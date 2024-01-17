c9df17160b58a169901cfa4dcd97b914e8b7290a
Valério <valcorr29@gmail.com>
2024-01-17 11:21:27 -0300

    Retorno++

--- PrimeiroProjetoEmJava/src/Aulas iniciais/Retorno.java
+++ PrimeiroProjetoEmJava/src/Aulas iniciais/Retorno.java
@@ -2,7 +2,10 @@ public class Retorno {
     public static void main(String[] args) {
         //uso de return para métodos
         int resultado = soma(32, 10);
+        //uso de return dentro de um ternary op.
+        boolean isTrue = (resultado>100)?true:false;
         System.err.println("Resultado: "+resultado);
+        System.err.println("Resultado: "+isTrue);
     }
 
     static int soma(int a,int b){
