b5ea39307886a08259653ef73ed7326055edb107
Valério <valcorr29@gmail.com>
2024-01-16 23:10:48 -0300

    Assets

---
+++ PrimeiroProjetoEmJava/src/Aulas iniciais/AssertFunc.java
@@ -0,0 +1,13 @@
+public class AssertFunc {
+    // assert serve para verificar uma condição e lançar uma exceção caso seja falsa
+    // útil pra depuração mas não aparece no terminal a menos que -ea esteja habilitado
+    // Dica: compile o arquivo com javac e depois ao invês de abrir com java arquivo use java -ea arquivo
+
+        public static void main(String[] args) {
+            int numero = -5;
+            assert (numero >= 0) : "Número não pode ser negativo";
+            System.out.println("O programa continua...");
+        }
+
+    
+}
