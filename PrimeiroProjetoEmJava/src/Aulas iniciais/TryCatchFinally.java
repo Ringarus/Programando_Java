f953ebe3db4ae5acf3a98bcb20d421ba1182abbd
Valério <valcorr29@gmail.com>
2024-01-16 23:15:55 -0300

    Try-catch-finally

---
+++ PrimeiroProjetoEmJava/src/Aulas iniciais/TryCatchFinally.java
@@ -0,0 +1,24 @@
+/**
+ * TryCatchFinally
+ */
+public class TryCatchFinally {
+    /*
+     * O try serve pra executar um bloco que pode vir a gerar uma exceção
+     * O catch pega esse erro e o trata
+     * O finally executa com ou sem exceção, ou seja, sempre
+     */
+    public static void main(String[] args) {
+        try {
+            // Bloco de código que pode gerar uma exceção]
+            // Aqui deve ser criado um ZeroDivisionError
+            int resultado = 10 / 0;
+        } catch (ArithmeticException e) {
+            // Trata a exceção
+            System.out.println("Erro aritmético: " + e.getMessage());
+        } finally {
+            // Bloco de código que será executado sempre, ocorrendo ou não uma exceção
+            System.out.println("Bloco finally");
+        }
+        
+    }
+}
