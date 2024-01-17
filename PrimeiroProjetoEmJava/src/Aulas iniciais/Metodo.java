b0543f39ae8e4de20bf578b63739e347faef4e45
Valério <valcorr29@gmail.com>
2024-01-16 23:30:35 -0300

    Métodos

---
+++ PrimeiroProjetoEmJava/src/Aulas iniciais/Metodo.java
@@ -0,0 +1,9 @@
+public class Metodo {
+    public static void main(String[] args) {
+        dizerOi("Fernanda");
+    }
+
+    static void dizerOi(String nome){
+        System.out.println("Olá "+ nome);
+    }
+}
