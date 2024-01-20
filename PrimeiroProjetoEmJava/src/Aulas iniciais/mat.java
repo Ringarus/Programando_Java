44b05a0b9c0dc91b70fa62894cd4de7d85b9e8de
Valério <valcorr29@gmail.com>
2024-01-20 00:38:53 -0300

    classe math

--- PrimeiroProjetoEmJava/src/Aulas iniciais/mat.java
+++ PrimeiroProjetoEmJava/src/Aulas iniciais/mat.java
@@ -1,3 +1,10 @@
 public class mat {
-    
+    public static void main(String[] args) {
+      int x = Math.min(5, 2);
+      int y = Math.max(4, 10);
+      double z = Math.ceil(23.2);
+      double d = Math.floor(23.2);
+      double random = Math.random();
+      System.err.printf("x: %d\ny: %d\nz: %.2f\nd: %.2f\nrandom: %f\n",x,y,z,d,random);
+    }
 }
