d74efd23d0b2aadcb91a91e0b6597868ddcf69c4
Valério <valcorr29@gmail.com>
2024-01-21 14:44:39 -0300

    Arrays 2D

--- PrimeiroProjetoEmJava/src/Aulas iniciais/Arrays.java
+++ PrimeiroProjetoEmJava/src/Aulas iniciais/Arrays.java
@@ -15,8 +15,28 @@ public class Arrays {
         carros[3] = "Celta";
 
         //Vendo os resultados
-        for(int i=0;i<5;i++){
+        for(int i=0;i<4;i++){
             System.err.printf("Minha moto é: %s\nMeu carro é: %s\n\n",motos[i],carros[i]);
         }
+
+        //Fazendo array 2D
+        int[][] pares = new int[3][3];
+        pares[0][0] = 2;
+        pares[0][1] = 4;
+        pares[0][2] = 6;
+        pares[1][0] = 12;
+        pares[1][1] = 14;
+        pares[1][2] = 16;
+        pares[2][0] = 104;
+        pares[2][1] = 216;
+        pares[2][2] = 512;
+
+        par:
+            for (int index = 0; index < pares.length; index++) {
+                for (int i = 0; i < pares.length; i++) {
+                    System.err.println(pares[index][i]);
+                    if(pares[index][i]==216) break par;
+                }
+            }
     }
 }
